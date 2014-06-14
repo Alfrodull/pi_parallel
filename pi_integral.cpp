bc2db0da5ab9ec92d5c05ed9eed2e3653c70dbab
alfrodull <fanjl92@gmail.com>
2014-06-14 12:43:54 +0800

    串行程序完成

--- pi_integral.cpp
+++ pi_integral.cpp
@@ -1,1 +1,22 @@
- 
+#include <iostream>
+using namespace std; 
+
+const int numOfParts = 100000000; //求积分时分割的份数
+
+inline double integrand(double x) //求圆周率的被积函数选用的是4/(1+x^2)
+{
+	return 4.0 / (1.0 + x*x);
+}
+
+int main(void)
+{
+	//求被积函数在[0,1]的定积分，所得结果即为圆周率
+	double pi = 0;
+	for (int i = 0; i < numOfParts; ++i)
+	{
+		double x = 1.0*i/numOfParts;
+		pi += integrand(x)*1.0/numOfParts;
+	}
+	cout << "Pi = " << pi << endl; 
+	return 0;
+} 
--- pi_prob.cpp
+++ pi_prob.cpp
@@ -1,1 +1,37 @@
- 
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+using namespace std; 
+
+const int numOfPoints = 1000000000;
+
+bool hitCircle(double x,double y)//计算坐标(x,y)的点是否在圆心为(1,1)半径为1的圆中
+{
+	double sqr_distance = (x - 1.0)*(x -  1.0) + (y - 1.0)*(y - 1.0);
+	if (sqr_distance > 1.0)//距离的平方大于半径平方
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+int main(void)
+{
+	unsigned seed = (unsigned)time(NULL);
+	int hit = 0;
+	for (int i = 0; i < numOfPoints; ++i)
+	{
+		double x = 2.0*rand_r(&seed)/(RAND_MAX);
+		double y = 2.0*rand_r(&seed)/(RAND_MAX);
+		if (hitCircle(x,y))
+		{
+			hit += 1;
+		}
+	}
+	// cout << hit << "/" << numOfPoints << endl;
+	cout << "Pi = " << ((double)hit)/numOfPoints*4.0 << endl;
+	return 0;
+}
